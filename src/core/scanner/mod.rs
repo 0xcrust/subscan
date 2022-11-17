5f54d89ed789a6593d561c31ad9e0b1d775f40b2
crustydev <ademola1145@gmail.com>
2022-11-17 07:51:33 +0100

    Write return functions for each scan type

--- src/core/scanner/mod.rs
+++ src/core/scanner/mod.rs
@@ -13,22 +13,27 @@ pub fn get_scanners() -> Vec<Box<dyn SubdomainScanner>> {
     ];
 }
 
+#[allow(dead_code)]
 pub fn bruteforce_scan() -> Box<dyn SubdomainScanner>{
     Box::new(methods::BruteForceScan::new())
 }
 
+#[allow(dead_code)]
 pub fn crtsh_scan() ->Box<dyn SubdomainScanner> {
     Box::new(methods::CrtShScan::new())
 }
 
+#[allow(dead_code)]
 pub fn threatcrowd_scan() -> Box<dyn SubdomainScanner> {
     Box::new(methods::ThreatCrowdScan::new())
 }
 
+#[allow(dead_code)]
 pub fn threatminer_scan() -> Box<dyn SubdomainScanner> {
     Box::new(methods::ThreatMinerScan::new())
 }
 
+#[allow(dead_code)]
 pub fn webarchive_scan() -> Box<dyn SubdomainScanner>{
     Box::new(methods::WebArchiveScan::new())
 }
