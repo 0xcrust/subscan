c89fa0084d809120fb9fc3af964eb5551eb94c7e
crustydev <ademola1145@gmail.com>
2022-11-17 11:52:45 +0100

    Serialize Subdomain struct for web response

--- src/core/models.rs
+++ src/core/models.rs
@@ -1,10 +1,11 @@
-#[derive(Debug, Clone)]
+use serde::Serialize;
+#[derive(Serialize, Debug, Clone)]
 pub struct Port {
     pub port: u16,
     pub conn_open: bool
 }
 
-#[derive(Debug, Clone)]
+#[derive(Serialize, Debug, Clone)]
 pub struct Subdomain {
     pub domain_name: String,
     pub open_ports: Vec<Port>,
