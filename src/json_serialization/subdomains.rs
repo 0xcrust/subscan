9a2056d406681bd133878c7b94d30c453482b2cd
crustydev <ademola1145@gmail.com>
2022-11-17 11:50:39 +0100

    Serialize subdomains vector for return as json

---
+++ src/json_serialization/subdomains.rs
@@ -0,0 +1,22 @@
+use serde::Serialize;
+use actix_web::{Error, HttpRequest, HttpResponse, Responder};
+use futures::future::{ready, Ready};
+
+use crate::core::models::Subdomain;
+
+#[derive(Serialize)]
+pub struct Subdomains {
+    pub subdomains: Vec<Subdomain>
+}
+
+impl Responder for Subdomains {
+    type Error = Error;
+    type Future = Ready<Result<HttpResponse, Error>>;
+
+    fn respond_to(self, _req: &HttpRequest) -> Self::Future {
+        let body = serde_json::to_string(&self).unwrap();
+        ready(Ok(HttpResponse::Ok()
+                .content_type("application/json")
+                .body(body)))
+    }
+}
